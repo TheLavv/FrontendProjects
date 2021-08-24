6f5be57cb14dc3c775c42a34f240d7980b3343b8
Andrey Golovatskiy <andrey.golovaczkiy@mail.ru>
2021-08-24 15:48:07 +0300

    some fixes

--- movie_search/src/Routes.tsx
+++ movie_search/src/Routes.tsx
@@ -5,7 +5,7 @@ import MovieInfo from './components/MovieInfo/MovieInfo'
 import MovieNotFound from './components/MovieNotFound/MovieNotFound'
 
 const Routes = () => (
-    <BrowserRouter basename="/movie_search/build/index.html">
+    <BrowserRouter>
         <Switch>
           <Route exact path='/' component={SearchPage} />
           <Route exact path='/movieInfo/:movieName' component={MovieInfo} />
